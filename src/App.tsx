79f816f4ef590f96d0a6fb3e50e82de817ad49f5
SSpectre <jordan.barron.hurst@gmail.com>
2025-09-13 02:46:40 -0400

    Added City, Building and GreatWork types

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,64 @@ import GreatWorksTable from './GreatWorksTable'
 import CitiesTable from './CitiesTable'
 import CivilizationSelect from './CivilizationSelect'
 
+type City = {
+    id: number;
+    name: string;
+    buildings: Building[];
+}
+
+type Building = {
+    id: number;
+    name: string;
+}
+
+type GreatWork = {
+    id: number;
+    name: string;
+    type: string;
+    civilization: string;
+    era: string;
+}
+
 export default function App() {
     const [cityNumber, setCityNumber] = useState(0);
     const [workNumber, setWorkNumber] = useState(0);
 
+    const [cities, setCities] = useState<City[]>([]);
+    const [buildings, setBuildings] = useState<Building[]>([]);
+
+    const [works, setWorks] = useState<GreatWork[]>([]);
+
     function addCity() {
+        setCities(
+            [
+                ...cities,
+                { id: cityNumber, name: "", buildings: [] }
+            ]
+        );
+
         setCityNumber(cityNumber + 1);
     }
 
     function removeCity() {
         if (cityNumber > 0) {
             setCityNumber(cityNumber - 1);
+
+            setCities(
+                cities.filter(city => city.id !== cityNumber)
+            );
         }
     }
 
     function addGreatWork() {
         if (cityNumber > 0) {
+            setWorks(
+                [
+                    ...works,
+                    { id: workNumber, name: "", type: "", civilization: "", era: "" }
+                ]
+            );
+
             setWorkNumber(workNumber + 1);
         }
         else {
@@ -30,12 +72,20 @@ export default function App() {
     function removeGreatWork() {
         if (workNumber > 0) {
             setWorkNumber(workNumber - 1);
+
+            setWorks(
+                works.filter(work => work.id !== workNumber)
+            );
         }
     }
 
     function reset() {
         setCityNumber(0);
         setWorkNumber(0);
+
+        setCities([]);
+        setBuildings([]);
+        setWorks([]);
     }
 
     return (
